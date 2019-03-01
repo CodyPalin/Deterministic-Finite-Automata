6bf17c971b713dd65d6e65c314e810b58e7564d4
CodyPalin <43323493+CodyPalin@users.noreply.github.com>
2019-02-28 17:40:10 -0700

    added a couple functions to DFAState
    addTransition() and doTransition() as well as some class variables.

--- dfa/fa/dfa/DFAState.java
+++ dfa/fa/dfa/DFAState.java
@@ -1,4 +1,3 @@
-package fa.dfa;
 import fa.State;
 
 /**
@@ -7,12 +6,41 @@ import fa.State;
  * @author Cody Palin, Dominick Edmonds
  */
 public class DFAState extends State {
-
+private int a = 0;//next element in array
+private String[] transitions;
+private DFAState[] states;
 	/**
 	 * Constructor for a DFAState, specifically to get the name
 	 * @param name
 	 */
 	public DFAState(String name){
 		this.name = name;
+		transitions = new String[8];
+		states = new DFAState[8];
+	}
+	
+	public void addTransition(String transition, DFAState state) {
+		/*
+		if(a+1 > transitions.length) {
+		}//change size of array when too large
+		*/
+		transitions[a]= transition;
+		states[a]= state;
+		a++;
+	}
+	
+	/**
+	 * @param transition to use
+	 * @return returns null if transition string not found, otherwise
+	 * returns the DFA state achieved by getting to that transition
+	 */
+	public DFAState doTransition(String transition) {
+		for(int i = 0;i<a;i++) {
+			if(transition.equals(transitions[i])) {
+				return states[i];
+			}
+		}
+		return null;
+		
 	}
 }
